91ce7eee97884f0e9c9ec8cd506521349800490d
Данил <79771746+MrDanilo3@users.noreply.github.com>
2023-05-04 01:06:44 +0300

    Add files via upload

---
+++ LB5_18_one/LB5_18_one/HoneyPot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB5_18_one
+{
+    internal class HoneyPot
+    {
+        private const int MaxCapacity = 5;
+        private readonly Queue<int> _portions = new Queue<int>();
+
+        public void AddPortion(int id)
+        {
+            Console.WriteLine("Bee {0} added a portion of honey", id);
+            _portions.Enqueue(id);
+        }
+
+        public void GetPortion()
+        {
+            if (_portions.Count == 0)
+            {
+                Console.WriteLine("No honey in the pot, the bear is waiting...");
+            }
+            else
+            {
+                int portion = _portions.Dequeue();
+                Console.WriteLine("Bear got a portion of honey from bee {0}", portion);
+            }
+        }
+
+        public void Refill()
+        {
+            Console.WriteLine("Bear refilled the pot with a portion of honey");
+            _portions.Enqueue(-1); // Добавление фиктивной порции меда для продолжения работы пчел
+        }
+
+        public bool IsFull()
+        {
+            return _portions.Count >= MaxCapacity;
+        }
+
+        public void Empty()
+        {
+            _portions.Clear();
+            Console.WriteLine("Honey pot is empty");
+        }
+
+        public void WakeUpBear()
+        {
+            Console.WriteLine("Wake up, bear!");
+        }
+    }
+
+}
+
---
+++ LB5_18_one/LB5_18_one/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace LB5_18_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створюємо горщик і ведмедя
+            HoneyPot pot = new HoneyPot();
+            Bear bear = new Bear(pot);
+
+            // Запускаємо ведмедя в окремому потоці
+            Thread bearThread = new Thread(bear.Start);
+            bearThread.Start();
+
+            // Створюємо бджіл і запускаємо їх у окремих потоках
+            int numBees = 5;
+            for (int i = 1; i <= numBees; i++)
+            {
+                Bee bee = new Bee(i, pot);
+                Thread beeThread = new Thread(bee.Start);
+                beeThread.Start();
+            }
+
+            // Очікуємо на закінчення роботи усіх потоків
+            bearThread.Join();
+            Console.WriteLine("Bear thread finished");
+            for (int i = 1; i <= numBees; i++)
+            {
+                string beeThreadName = string.Format("Bee {0} thread", i);
+                Thread beeThread = Thread.CurrentThread.Name == beeThreadName ? Thread.CurrentThread : new Thread(() => { });
+                beeThread.Join();
+                Console.WriteLine(beeThreadName + " finished");
+            }
+
+            Console.WriteLine("All threads finished");
+        }
+    }
+}
